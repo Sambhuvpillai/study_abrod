3565a82b9668712aada8955607cc56046063a063
sambhuvpillai <sambhuvpillai@gmail.com>
2022-06-30 16:30:30 +0530

    second commit

--- app1/urls.py
+++ app1/urls.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 urlpatterns = [
         path('',views.home, name='home' ),
-        path('studdashboard',views.studdashboard, name='studdashboard' )
+        # Eldho Mathew
+        path('newlyassignleads',views.newlyassignleads, name='newlyassignleads' ),
+        # Sambhu v pillai
+        path('currentleads',views.currentleads, name='currentleads' ),
+        path('previousleads',views.previousleads, name='previousleads' ),
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
+
+# counsilor dashboard
 def home(request):
     return render(request,'home.html')
 
-def studdashboard(request):
-    return render(request,'studdash.html')
-    
+def newlyassignleads(request):
+    return render(request, 'newly_leads.html')
+
+def currentleads(request):
+    return render(request, 'current_leads.html')
+
+def previousleads(request):
+    return render(request, 'previous_leads.html')
